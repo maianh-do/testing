4d8d354a4d63da159f7ceb3fe78ad9abf6a1b8d1
maianh-do <82898479+maianh-do@users.noreply.github.com>
2021-09-03 21:52:30 -0700

    Create ho3_5.py

---
+++ ho3_5.py
@@ -0,0 +1,25 @@
+# hands on 3, problem 4
+# Author: Nguyen Mai Anh Do
+# Last editted: 09/01/2021
+
+#Print the string - “PYTHON IS FUN”
+
+print("PYTHON IS FUN")
+
+#Assign the same string - “PYTHON IS FUN” - to a variable.
+variable = "PYTHON IS FUN"
+
+#print variable
+print(variable)
+
+#print length of the variable
+print(len(variable))
+
+#Print the variable in lower case by using the lower()method
+print(variable.lower())
+
+#Concatenate
+first_variable = "PYTHON IS FUN "+"because it's easy"
+
+#print concatenated string
+print(first_variable)
